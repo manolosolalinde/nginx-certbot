58bbdebe02d490972e0caeaae6a24cb4a03286f1
Manuel Solalinde <manolosolalinde@gmail.com>
2021-08-05 08:03:58 -0400

    fixed errors

--- create_env_vars.sh
+++ create_env_vars.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+#run as root
 echo "PRIVKEY_PEM: $PWD/data/certbot/conf/live/$MYDOMAIN/privkey.pem"
 echo "FULLCHAIN_PEM: $PWD/data/certbot/conf/live/$MYDOMAIN/fullchain.pem"
 
-sudo export FULLCHAIN_PEM=$PWD/data/certbot/conf/live/$MYDOMAIN/fullchain.pem
-sudo export PRIVKEY_PEM=$PWD/data/certbot/conf/live/$MYDOMAIN/privkey.pem
-sudo bash -c "echo 'export MYDOMAIN=$MYDOMAIN' > /etc/profile.d/setdomain_envs.sh"
-sudo bash -c "echo 'export FULLCHAIN_PEM=$PWD/data/certbot/conf/live/$MYDOMAIN/fullchain.pem' >> /etc/profile.d/setdomain_envs.sh"
-sudo bash -c "echo 'export PRIVKEY_PEM=$PWD/data/certbot/conf/live/$MYDOMAIN/privkey.pem' >> /etc/profile.d/setdomain_envs.sh"
-sudo chmod +x /etc/profile.d/setdomain_envs.sh
+export FULLCHAIN_PEM=$PWD/data/certbot/conf/live/$MYDOMAIN/fullchain.pem
+export PRIVKEY_PEM=$PWD/data/certbot/conf/live/$MYDOMAIN/privkey.pem
+bash -c "echo 'export MYDOMAIN=$MYDOMAIN' > /etc/profile.d/setdomain_envs.sh"
+bash -c "echo 'export FULLCHAIN_PEM=$PWD/data/certbot/conf/live/$MYDOMAIN/fullchain.pem' >> /etc/profile.d/setdomain_envs.sh"
+bash -c "echo 'export PRIVKEY_PEM=$PWD/data/certbot/conf/live/$MYDOMAIN/privkey.pem' >> /etc/profile.d/setdomain_envs.sh"
+chmod +x /etc/profile.d/setdomain_envs.sh
